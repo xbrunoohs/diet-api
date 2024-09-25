b32426134d496540c629953fa79792d7100a9dba
BRUNO HENRIQUE SOUZA <bruno_henrique_souza@estudante.sc.senai.br>
2024-09-25 20:34:18 -0300

    commit 2

--- App.js
+++ App.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('API funcionando!');
+});
+
 app.use('/api', userRoutes);
 app.use('/api', mealRoutes);
 
